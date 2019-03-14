47f8085ba2ed48a9b8badffa19ebc71a83600bc2
LouDupont <loudupont@outlook.fr>
2019-03-14 09:24:12 +0100

    oui

--- darthNihilus.php
+++ darthNihilus.php
@@ -2,61 +2,61 @@
 include('header.html')
 ?>
 
-    <!-- CONTENT START -->
-    <?php
-    include('navbar.php');
-    ?>
+<!-- CONTENT START -->
+<?php
+include('navbar.php');
+?>
 
-    <div class="container-fluid mt-3">
-        <div class="row">
-            <div class="col-md-4">
-                <img class="image" src="./img/darthNihilusPage.jpg" alt="Darth Nihilus">
-                <p class="mt-5">"-Oh tu ignores tout de moi ce que je suis , ma vision des choses , cette plaie de la
-                    force que tu as comblée."</p>
-            </div>
+<div class="container-fluid mt-3">
+    <div class="row">
+        <div class="col-md-4">
+            <img class="image" src="./img/darthNihilusPage.jpg" alt="Darth Nihilus">
+            <p class="mt-5">"-Oh tu ignores tout de moi ce que je suis , ma vision des choses , cette plaie de la
+                force que tu as comblée."</p>
+        </div>
 
-            <div class="col-md-4 bordel">
-                <p>Dark Nihilus est connu sous le nom de seigneur de la faim , le pire cauchemar des jedi et le
-                    destructeur de Katarr , c'est par ailleur lui-même qui débuta la Première Purge Jedi en -3954 av.BY
-                    avant de se faire tuer en -3951 av.BY , c'est le seul seigneur noir à avoir jamais jeté son dévolu
-                    sur la galaxie tout entière.</p>
+        <div class="col-md-4 bordel">
+            <p>Dark Nihilus est connu sous le nom de seigneur de la faim , le pire cauchemar des jedi et le
+                destructeur de Katarr , c'est par ailleur lui-même qui débuta la Première Purge Jedi en -3954 av.BY
+                avant de se faire tuer en -3951 av.BY , c'est le seul seigneur noir à avoir jamais jeté son dévolu
+                sur la galaxie tout entière.</p>
 
-                <p>On ne sait quasiment rien du passé de Dark Nihilus , s'il était un sith ou un jedi voire un membre
-                    de l'Empire Sith de Dark Revan, en tout cas une chose est sûre il a participé à la Première Guerre
-                    Civile des Jedi quelques années plus tôt même si son rôle dans ce conflit, destructeur reste
-                    inconnu pour l'instant sauf qu'il aurait subit une terrible plaie dans la force quelques années
-                    plus tôt</p>
+            <p>On ne sait quasiment rien du passé de Dark Nihilus , s'il était un sith ou un jedi voire un membre
+                de l'Empire Sith de Dark Revan, en tout cas une chose est sûre il a participé à la Première Guerre
+                Civile des Jedi quelques années plus tôt même si son rôle dans ce conflit, destructeur reste
+                inconnu pour l'instant sauf qu'il aurait subit une terrible plaie dans la force quelques années
+                plus tôt</p>
 
-                <p>Mais alors que Dark Traya voulait détruire la force, Dark Nihilus et Dark Sion s'allièrent pour
-                    vaincre leur ancien maître et coupèrent son lien avec la force. Débarrassés de leur rivale, les
-                    deux Seigneurs Noirs des Sith prirent le contrôle du Triumvirat Sith à partir des restes de
-                    l'Empire Sith de Dark Revan, et se séparèrent afin de commençer la 1°Purge des Jedi en -3954 av.BY,
-                    alors débuta une longue traque , Dark Nihilus attaquant les places-fortes de l'Ordre Jedi forçant
-                    ceux-ci à fuir le Temple Jedi sur Coruscant, ne pouvant plus former d'apprentis sans prendre des
-                    risques inutiles. Il massacra un nombre indénombrable de jedi, détruisant mondes après mondes.</p>
-            </div>
+            <p>Mais alors que Dark Traya voulait détruire la force, Dark Nihilus et Dark Sion s'allièrent pour
+                vaincre leur ancien maître et coupèrent son lien avec la force. Débarrassés de leur rivale, les
+                deux Seigneurs Noirs des Sith prirent le contrôle du Triumvirat Sith à partir des restes de
+                l'Empire Sith de Dark Revan, et se séparèrent afin de commençer la 1°Purge des Jedi en -3954 av.BY,
+                alors débuta une longue traque , Dark Nihilus attaquant les places-fortes de l'Ordre Jedi forçant
+                ceux-ci à fuir le Temple Jedi sur Coruscant, ne pouvant plus former d'apprentis sans prendre des
+                risques inutiles. Il massacra un nombre indénombrable de jedi, détruisant mondes après mondes.</p>
+        </div>
 
-            <div class="col-md-4">
-                <h2>Informations biographiques</h2>
-                <p>Décès: 3951 av.BY, au dessus de Telos IV</p>
-                <h2>Description physique</h2>
-                <p>Escpèce : Humain</p>
-                <p>Genre : Masculin</p>
-                <h2>Informations politiques et fonctions</h2>
-                <p>Position : Seigneur Noir des Sith</p>
-                <p>Affiliation : Triumvirate Sith</p>
-                <p>Maîtres : Darth Traya</p>
-                <p>Apprentis : Visas Marr</p>
-                <ul class="yellow">Armes :
-                    <li class="yellow">Sabre laser</li>
-                    <li class="yellow">La Force</li>
-                    <li class="yellow">Foudre Sith</li>
-                </ul>
-            </div>
+        <div class="col-md-4">
+            <h2>Informations biographiques</h2>
+            <p>Décès: 3951 av.BY, au dessus de Telos IV</p>
+            <h2>Description physique</h2>
+            <p>Escpèce : Humain</p>
+            <p>Genre : Masculin</p>
+            <h2>Informations politiques et fonctions</h2>
+            <p>Position : Seigneur Noir des Sith</p>
+            <p>Affiliation : Triumvirate Sith</p>
+            <p>Maîtres : Darth Traya</p>
+            <p>Apprentis : Visas Marr</p>
+            <ul class="yellow">Armes :
+                <li class="yellow">Sabre laser</li>
+                <li class="yellow">La Force</li>
+                <li class="yellow">Foudre Sith</li>
+            </ul>
         </div>
     </div>
+</div>
 
-    <!-- CONTENT END -->
+<!-- CONTENT END -->
 <?php
 include('footer.html');
 ?>
--- darthSion.php
+++ darthSion.php
@@ -5,61 +5,61 @@ include('header.html');
 <?php
 include('navbar.php');
 ?>
-    <div class="container-fluid mt-5">
-        <div class="row">
-            <div class="col-md-4">
-                <img class="image" src="./img/darthSionPage.jpg" alt="Darth Nihilus">
-                <p class="mt-5">"Revenir était de la folie ! Je vous ai épargnée une fois ! Je ne compte pas refaire la même erreur !"</p>
-            </div>
+<div class="container-fluid mt-5">
+    <div class="row">
+        <div class="col-md-4">
+            <img class="image" src="./img/darthSionPage.jpg" alt="Darth Nihilus">
+            <p class="mt-5">"Revenir était de la folie ! Je vous ai épargnée une fois ! Je ne compte pas refaire la même erreur !"</p>
+        </div>
 
-            <div class="col-md-4 bordel">
-                <p>Dark Sion, le Seigneur de la Douleur, a été l'un des plus puissants Seigneur Sith ayant vécu aux
-                    alentours de 4000 av.BY. Les détails qui entourent son existence et surtout son passé restent pour
-                    le moins ombrageux, à vrai dire on ne sait pas grand-chose de lui si ce n'est qu'il a connu la
-                    tragédie de Malachor V, à l'instar de Revan, Malak et de nombreux autres Jedi entraînes de façon
-                    irréfléchie dans la sanglante croisade contre les guerriers mandaloriens.</p>
+        <div class="col-md-4 bordel">
+            <p>Dark Sion, le Seigneur de la Douleur, a été l'un des plus puissants Seigneur Sith ayant vécu aux
+                alentours de 4000 av.BY. Les détails qui entourent son existence et surtout son passé restent pour
+                le moins ombrageux, à vrai dire on ne sait pas grand-chose de lui si ce n'est qu'il a connu la
+                tragédie de Malachor V, à l'instar de Revan, Malak et de nombreux autres Jedi entraînes de façon
+                irréfléchie dans la sanglante croisade contre les guerriers mandaloriens.</p>
 
-                <p>Tout ce que l'on sait sur Sion est qu'il a passé sa vie à combattre l'Ordre Jedi. Jadis jeune
-                    Padawan indiscipliné sur Ossus, il ne tarda pas à trahir pour devenir un Maraudeur au service de la
-                    "Confrérie Sith" d'Exar Kun, durant la Grande Guerre des Sith. Après la défaite, il partit en
-                    cavale à l'instar de plusieurs autres confrères. Il servit notamment de mercenaire aux Mandaloriens
-                    jusqu'aux Guerres mandaloriennes, puis se rallia à Dark Revan pendant la Guerre Civile des Jedi.</p>
+            <p>Tout ce que l'on sait sur Sion est qu'il a passé sa vie à combattre l'Ordre Jedi. Jadis jeune
+                Padawan indiscipliné sur Ossus, il ne tarda pas à trahir pour devenir un Maraudeur au service de la
+                "Confrérie Sith" d'Exar Kun, durant la Grande Guerre des Sith. Après la défaite, il partit en
+                cavale à l'instar de plusieurs autres confrères. Il servit notamment de mercenaire aux Mandaloriens
+                jusqu'aux Guerres mandaloriennes, puis se rallia à Dark Revan pendant la Guerre Civile des Jedi.</p>
 
-                <p>Au milieu du chaos qui suivit la chute de Dark Malak, Sion rencontra Dark Traya avec Dark Nihilus,
-                    qui lui enseignèrent davantage de connaissances sur la Force. Cela lui permit notamment de devenir
-                    immortel, ainsi il ressuscitait à chaque fois qu'il mourrait lors d'un combat ou tout autre
-                    situation ce qui l'avantageait énormément.</p>
+            <p>Au milieu du chaos qui suivit la chute de Dark Malak, Sion rencontra Dark Traya avec Dark Nihilus,
+                qui lui enseignèrent davantage de connaissances sur la Force. Cela lui permit notamment de devenir
+                immortel, ainsi il ressuscitait à chaque fois qu'il mourrait lors d'un combat ou tout autre
+                situation ce qui l'avantageait énormément.</p>
 
-                <p>Par la suite lui et Nihilus complotèrent contre Traya, se retournèrent contre elle et lui coupèrent
-                    tout lien avec la force. Bien que celle-ci parvint à s'échapper, les seigneurs Sith se mirent
-                    d'accord pour anéantir l'Ordre Jedi tandis que Sion et ses assassins Siths se séparèrent dans la
-                    chasse aux Jedi.</p>
-            </div>
+            <p>Par la suite lui et Nihilus complotèrent contre Traya, se retournèrent contre elle et lui coupèrent
+                tout lien avec la force. Bien que celle-ci parvint à s'échapper, les seigneurs Sith se mirent
+                d'accord pour anéantir l'Ordre Jedi tandis que Sion et ses assassins Siths se séparèrent dans la
+                chasse aux Jedi.</p>
+        </div>
 
-            <div class="col-md-4">
-                <h2>Informations biographiques</h2>
-                <p>Décès: 3951 av.BY, Malachor V</p>
-                <h2>Description physique</h2>
-                <p>Escpèce : Humain</p>
-                <p>Genre : Masculin</p>
-                <h2>Informations politiques et fonctions</h2>
-                <p>Position : Seigneur Noir des Sith</p>
-                <ul class="yellow">Affiliation :
-                    <li class="yellow">Empire Sith d'Exar Kun</li>
-                    <li class="yellow">Empire Sith de Revan</li>
-                    <li class="yellow">Triumvirate Sith</li>
-                </ul>
-                <p>Maîtres : Darth Traya</p>
-                <p>Apprentis : Visas Marr</p>
-                <ul class="yellow">Armes :
-                    <li class="yellow">Sabre laser</li>
-                    <li class="yellow">La Force</li>
-                </ul>
-            </div>
+        <div class="col-md-4">
+            <h2>Informations biographiques</h2>
+            <p>Décès: 3951 av.BY, Malachor V</p>
+            <h2>Description physique</h2>
+            <p>Escpèce : Humain</p>
+            <p>Genre : Masculin</p>
+            <h2>Informations politiques et fonctions</h2>
+            <p>Position : Seigneur Noir des Sith</p>
+            <ul class="yellow">Affiliation :
+                <li class="yellow">Empire Sith d'Exar Kun</li>
+                <li class="yellow">Empire Sith de Revan</li>
+                <li class="yellow">Triumvirate Sith</li>
+            </ul>
+            <p>Maîtres : Darth Traya</p>
+            <p>Apprentis : Visas Marr</p>
+            <ul class="yellow">Armes :
+                <li class="yellow">Sabre laser</li>
+                <li class="yellow">La Force</li>
+            </ul>
         </div>
     </div>
+</div>
 
-    <!-- CONTENT END -->
+<!-- CONTENT END -->
 <?php
 include('footer.html');
 ?>
